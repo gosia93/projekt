b67085ad5bb857a5ad10e4fb44f2bda6288fa9e5
Małgorzata Koselak <gosia.koselak@gmail.com>
2015-08-13 18:27:11 +0200

    voluntary types

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-r1 = Role.create({name: "Wolontariusz"})
-r2 = Role.create({name: "Organjzacja"})
-r3 = Role.create({name: "Osoba prywatna"})
+
+
+v1 = VolontaryType.create({name:"praca z dziećmi"})
+v2 = VolontaryType.create({name:"praca z osobami starszymi"})
+v3 = VolontaryType.create({name:"praca z osobami niepełnosprawnymi"})
+v4 = VolontaryType.create({name:"praca w hospicjum"})
+v5 = VolontaryType.create({name:"praca w ośrodkach specjalnych"})
+v6 = VolontaryType.create({name:"praca z osobami uzależnionymi"})
+v7 = VolontaryType.create({name:"projekty kulturalne"})
+v8 = VolontaryType.create({name:"prjekty edukacyjne"})
+v9 = VolontaryType.create({name:"projekty ekologiczne" })
+v10 = VolontaryType.create({name:"pomoc zwierzętom"})
+v11 = VolontaryType.create({name:"projekty sportowe"})
+v12 = VolontaryType.create({name:"inne"})
+
 
 
 admin = User.create({login: "admin", role_id: "4" , email:"strefa_wolontariatu@poczta.fm", password:"strefa123"})
