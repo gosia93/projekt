5dc01b51cce87f41a6a611fb9c28a880aefca6e9
Malgorzata Koselak <12_koselak@leszczyna.(none)>
2015-08-14 15:59:30 +0200

    change ing registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,11 +8,11 @@ class RegistrationsController < Devise::RegistrationsController
 
  	def after_sign_up_path_for(resource)
     	if @user.role_id == 1
-    		"/volonteers/new"
+    		"/12_koselak/wolontariat/volonteers/new"
     	elsif @user.role_id == 2
-    		"/organisations/new"
+    		"/12_koselak/wolontariat/organisations/new"
     	elsif @user.role_id == 3
-    		"/individuals/new"
+    		"/12_koselak/wolontariat/individuals/new"
     	end
 	end
 end
